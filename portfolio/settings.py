675a840a24fdd0ee64de7773b5964770e7523500
Ahmed Bilal <nagorib588@gmail.com>
2026-09-07 17:00:41 +0530

    Fix static files configuration

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -122,9 +122,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+# Static files (CSS, JavaScript, Images)
+
+STATIC_URL = "/static/"
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
